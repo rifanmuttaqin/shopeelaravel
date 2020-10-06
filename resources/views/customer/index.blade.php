2f20428069d19213bc3501383234f9bd473d5c63
Rifan Muttaqin <rifanmuttaqin@gmail.com>
2020-10-06 14:37:13 +0700

    commit 19
    action :

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -68,10 +68,57 @@
 
 @endsection
 
+@section('modal')
+
+
+<div class="modal fade" id="waModal" role="dialog">
+  <div class="modal-dialog modal-md">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      
+        <div class="modal-body">
+     
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" class="form-control" disabled id="username">
+          </div>
+
+          <div class="form-group">
+            <label>Nomor</label>
+            <input type="text" class="form-control" disabled value="" id="nomor">
+          </div>
+
+          <div class="form-group">
+            <label>Isi Pesan Singkat</label>
+            <textarea type="text" class="form-control" value="" id="pesan"> </textarea>
+          </div>
+
+          <div class="form-group">
+            <small> * Pastikan anda telah login menggunakan whatshapp pada Komputer ini </small>
+          </div>
+
+        </div>
+      
+        <div class="modal-footer">
+          <div class="form-group">
+            <button type="button" id="kirim_pesan" class="btn btn-info btn-default pull-left">Kirim Pesan WA</button>
+          </div>
+        </div>       
+
+    </div>
+  </div>
+</div>
+
+@endsection
+
 @push('scripts')
 <script type="text/javascript">
 
 $(function () {
+
+  // Datatables
   table = $('#customer_table').DataTable({
       processing: true,
       serverSide: true,
@@ -88,6 +135,37 @@ $(function () {
           {data: 'sum_order', name: 'sum_order'},
       ]
   });
+
+  // Row Click event
+  $('.dataTable').on('click', 'tbody tr', function() {
+      
+      var data = table.row(this).data();
+
+      $('#username').val(data.username_pembeli);
+      $('#nomor').val(data.telfon_pembeli);
+      $('#pesan').val(null);
+
+
+      $('#waModal').modal('toggle');
+  })
+
+
+  // Kirim Pesan
+
+  $( "#kirim_pesan" ).click(function() {
+
+    var pesan = $('#pesan').val();
+    var nomor = $('#nomor').val();
+
+    var url   = "https://wa.me/:nomor?text=:pesan";
+    url       = url.replace(':nomor', nomor);
+    url       = url.replace(':pesan', pesan);
+
+    window.open(url);
+
+
+  });
+
 });
 
 </script>
