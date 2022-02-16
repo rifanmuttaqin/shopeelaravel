64063807dc2acd4e0595da77f603f51610cf770b
Rifan Muttaqin <rifanmuttaqin@gmail.com>
2022-02-16 14:30:27 +0700

    commit #113
    action :+1:

--- Modules/Pemasukan/Resources/views/transaksi/search.blade.php
+++ Modules/Pemasukan/Resources/views/transaksi/search.blade.php
@@ -126,6 +126,9 @@ function changeStatus(param)
     });
 }
 
+function cb(start, end) {
+    $('#dates span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+}
 
 function tampilPreview()
 {
@@ -151,6 +154,9 @@ function tampilPreview()
 }
 
 $(function () {
+    
+    var start = moment().subtract(29, 'days');
+    var end = moment();
 
     $('#nama_customer').select2({
         allowClear: true,
@@ -173,7 +179,20 @@ $(function () {
         }
     })
 
-    $('input[name="dates"]').daterangepicker();
+    $('input[name="dates"]').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
 
     $( "#tampil" ).click(function() {
         tampilPreview();
