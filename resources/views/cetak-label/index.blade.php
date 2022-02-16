402fecda10ffb442c4eab42abd9582a8cba25335
Rifan Muttaqin <rifanmuttaqin@gmail.com>
2022-02-16 14:47:41 +0700

    commit #104
    action : perbaikan date range

--- resources/views/cetak-label/index.blade.php
+++ resources/views/cetak-label/index.blade.php
@@ -134,8 +134,15 @@
 
 <script type="text/javascript">
 
+function cb(start, end) {
+    $('#dates span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+}
+
+
 $( document ).ready(function() {
 
+	var start = moment().subtract(29, 'days');
+    var end = moment();
 
     // select2-class
     $('#toko').select2({
@@ -193,7 +200,20 @@ $( document ).ready(function() {
 
     });
     
-    $('input[name="dates"]').daterangepicker();
+    $('input[name="dates"]').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
 
     $('#previewData').click(function() {
         
